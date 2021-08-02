a3c0d25b78cbe383988458e8121a19708455b6a3
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-08-02 12:16:18 +0100

    First go program

---
+++ codecademy/Introduction/structure.go
@@ -0,0 +1,11 @@
+// Below line is package declaration which thus creates executable file 
+// Also auto invokes the main function below when running the code
+package main
+// Import fmt package
+import "fmt"
+
+// Define main function
+func main () {
+  // Use Println function of fmt package to print
+  fmt.Println("Hello There!")
+}
