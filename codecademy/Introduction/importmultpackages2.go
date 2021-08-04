c1e00cf6a2a3b1d5b60fe8f5174701640dcbefe3
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-08-04 15:59:19 +0100

    Added project and 2 other files from intro

---
+++ codecademy/Introduction/importmultpackages2.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+// Import time package as shorthand t
+import t "time"
+
+func main() {
+  // Use Now() function from time package to print time now
+	fmt.Println(t.Now())
+}
---
+++ codecademy/Introduction/project.go
@@ -0,0 +1,21 @@
+// ASCI art
+
+// Main package declaration
+package main
+// Import fmt package
+import "fmt"
+
+func main() {
+  fmt.Println("    `.-::::::-.`    ")
+  fmt.Println(".:-::::::::::::::-:.")
+  fmt.Println("`_:::    ::    :::_`")
+  fmt.Println(" .:( ^   :: ^   ):. ")
+  fmt.Println(" `:::   (..)   :::. ")
+  fmt.Println(" `:::::::UU:::::::` ")
+  fmt.Println(" .::::::::::::::::. ")
+  fmt.Println(" O::::::::::::::::O ")
+  fmt.Println(" -::::::::::::::::- ")
+  fmt.Println(" `::::::::::::::::` ")
+  fmt.Println("  .::::::::::::::.  ")
+  fmt.Println("    oO:::::::Oo     ")
+}
